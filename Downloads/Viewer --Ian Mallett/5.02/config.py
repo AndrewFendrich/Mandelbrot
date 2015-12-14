0c9c2f215c2f5bcdea073b1ba07f2949445da353
Andrew Fendrich <afendrich@gmail.com>
2015-12-14 18:16:27 -0500

    Change Configuration file from 100 to 10,000 iterations change use_double_precision from False to True change screen_size to 32,240

--- Downloads/Viewer --Ian Mallett/5.02/config.py
+++ Downloads/Viewer --Ian Mallett/5.02/config.py
@@ -1,5 +1,5 @@
 #Initial screen resolution
-screen_size = [800,600]
+screen_size = [320,240]
 
 #Initial bounds of real axis
 bounds_x = [-2.0,1.0]
@@ -9,7 +9,7 @@ bounds_y = [-1.0,1.0]
 #Will allow the zoom to go much deeper, but requires a newer GPU.
 #   With single-precision, can get up to 50,000x zoom.  With double
 #   precision, can get up to 10,000,000,000,000x zoom.
-use_double_precision = False
+use_double_precision = True
 
 #Smooth through the bands of color
 use_smooth_shading = False
@@ -19,12 +19,12 @@ smooth_shading_scale = 1.0
 
 #For smooth shading, the iterations will need to be higher for a
 #   smaller shading scale.
-iterations = 100
+iterations = 10000
 
 #Zoom speed
 #   Closer to 1.0 is slower zooming
 #   Closer to 0.0 is faster zooming
-zoom_scalar = 0.98
+zoom_scalar = 0.85
 
 #The amount of work done goes up quadratically with this number!  Compiling the shader will
 #   also take increasingly longer as this increases (maybe also quadratic).
