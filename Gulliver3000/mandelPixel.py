fd2eb5d5a7cebb9657c592f89aaf6ed281875e70
Andrew Fendrich <afendrich@gmail.com>
2016-01-28 09:03:17 -0500

    Create pixel class

--- Gulliver3000/mandelPixel.py
+++ Gulliver3000/mandelPixel.py
@@ -1,8 +1,22 @@
-def mandel_pixel(c,n = 16):
-    z = c   
-    for i in range(n):
-        a = z * z
-        z=a + c
-        if a.real  >= 4.:            
-            return(i)
-    return(0)
+"""
+
+mandelbrot pixel class
+
+"""
+#import math
+
+class mandelPixel():
+    def __init__(self,coord = (0,0),iter=0):
+        self.coordinate = coord
+        self.iteration = iter
+        self.z = 0        
+        
+    def iterate(self,limit):   
+        for i in range(limit):
+            a = self.z * self.z
+            self.z = a + self.coordinate
+            self.iteration = self.iteration + 1
+            print("Iteraion")
+            if a.real  >= 4.0:
+                print("Escaped")
+                break
